7c031707e4e71e6b1ae3aee9477abf9e19cd922d
Ryan Tanay <ryantanay@newevolution.org>
2016-12-27 15:30:47 -0600

    Implemented not found response

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -55,6 +55,11 @@ defmodule KVServer do
     :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
   end
 
+  defp write_line(socket, {:error, :not_found}) do
+    # Known error. Write to the client.
+    :gen_tcp.send(socket, "NOT FOUND\r\n")
+  end
+
   defp write_line(_socket, {:error, :closed}) do
     # The connection was closed, exit politely.
     exit(:shutdown)
