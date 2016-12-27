2f059f2360d91e91b36d20d14ae604ae55994c69
Ryan Tanay <ryantanay@newevolution.org>
2016-12-27 13:52:27 -0600

    Implemented simple echoing TCP server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,5 +1,6 @@
 defmodule KVServer do
   use Application
+  require Logger
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
@@ -8,5 +9,35 @@ defmodule KVServer do
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
