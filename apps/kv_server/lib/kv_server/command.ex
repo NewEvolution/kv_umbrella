d0c8473019d709c77f7b1ade11162f1452e4f10e
Ryan Tanay <ryantanay@newevolution.org>
2017-01-03 11:23:33 -0600

    Modified commands to take registry as argument

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -42,36 +42,36 @@ defmodule KVServer.Command do
   @doc """
   Runs the given command.
   """
-  def run(command)
+  def run(command, pid)
 
-  def run({:create, bucket}) do
-    KV.Registry.create(KV.Registry, bucket)
+  def run({:create, bucket}, pid) do
+    KV.Registry.create(pid, bucket)
     {:ok, "OK\r\n"}
   end
 
-  def run({:get, bucket, key}) do
-    lookup bucket, fn pid ->
+  def run({:get, bucket, key}, pid) do
+    lookup bucket, pid, fn pid ->
       value = KV.Bucket.get(pid, key)
       {:ok, "#{value}\r\nOK\r\n"}
     end
   end
 
-  def run({:put, bucket, key, value}) do
-    lookup bucket, fn pid ->
+  def run({:put, bucket, key, value}, pid) do
+    lookup bucket, pid, fn pid ->
       KV.Bucket.put(pid, key, value)
       {:ok, "OK\r\n"}
     end
   end
 
-  def run({:delete, bucket, key}) do
-    lookup bucket, fn pid ->
+  def run({:delete, bucket, key}, pid) do
+    lookup bucket, pid, fn pid ->
       KV.Bucket.delete(pid, key)
       {:ok, "OK\r\n"}
     end
   end
 
-  defp lookup(bucket, callback) do
-    case KV.Registry.lookup(KV.Registry, bucket) do
+  defp lookup(bucket, pid, callback) do
+    case KV.Registry.lookup(pid, bucket) do
       {:ok, pid} -> callback.(pid)
       :error -> {:error, :not_found}
     end
