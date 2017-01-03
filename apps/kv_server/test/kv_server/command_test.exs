6c536e800f28d6e0a89f4079f07f9a6de7e234be
Ryan Tanay <ryantanay@newevolution.org>
2017-01-03 15:10:31 -0600

    Wrote unit tests for KVServer.Command.run functionality

--- apps/kv_server/test/kv_server/command_test.exs
+++ apps/kv_server/test/kv_server/command_test.exs
@@ -1,4 +1,34 @@
 defmodule KVServer.CommandTest do
   use ExUnit.Case, async: true
+
   doctest KVServer.Command
+
+  setup context do
+    {:ok, _} = KV.Registry.start_link(context.test)
+    {:ok, registry: context.test}
+  end
+
+  test "run creates new buckets", %{registry: registry} do
+    assert {:error, :not_found} = KVServer.Command.run({:put, "shopping", "eggs", 12}, registry)
+
+    KVServer.Command.run({:create, "shopping"}, registry)
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:put, "shopping", "eggs", 12}, registry)
+  end
+
+  test "run adds and retrieves values from buckets", %{registry: registry} do
+    KVServer.Command.run({:create, "shopping"}, registry)
+
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:put, "shopping", "eggs", 12}, registry)
+
+    assert {:ok, "12\r\nOK\r\n"} = KVServer.Command.run({:get, "shopping", "eggs"}, registry)
+  end
+
+  test "run deletes values from buckets", %{registry: registry} do
+    KVServer.Command.run({:create, "shopping"}, registry)
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:put, "shopping", "eggs", 12}, registry)
+    assert {:ok, "12\r\nOK\r\n"} = KVServer.Command.run({:get, "shopping", "eggs"}, registry)
+
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:delete, "shopping", "eggs"}, registry)
+    assert {:error, :not_found} = KVServer.Command.run({:get, "shopping", "eggs"}, registry)
+  end
 end
